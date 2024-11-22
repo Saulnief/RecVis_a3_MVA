ec9e38adec84a6ceacbd31eb5165d78133d6d273
Saulnief <francois.saulnier@student.isae-supaero.fr>
2024-11-22 09:36:00 +0100

    Add files via upload

---
+++ model.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+from torchvision.models import resnet50, ResNet50_Weights
+
+nclasses = 500
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv3 = nn.Conv2d(20, 20, kernel_size=5)
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, nclasses)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(F.max_pool2d(self.conv3(x), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
+    
+class ResNet50(nn.Module):
+    def __init__(self):
+        super(ResNet50, self).__init__()
+        self.model_resnet50 = models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+        self.model_resnet50.fc = nn.Linear(2048, nclasses)
+        
+    def forward(self, x):
+        return self.model_resnet50(x)
---
+++ model_factory.py
@@ -0,0 +1,36 @@
+"""Python file to instantite the model and the transform that goes with it."""
+
+from data import data_transforms, data_transforms_resnet    
+from model import Net, ResNet50
+
+
+class ModelFactory:
+    def __init__(self, model_name: str):
+        self.model_name = model_name
+        self.model = self.init_model()
+        self.transform = self.init_transform()
+
+    def init_model(self):
+        if self.model_name == "basic_cnn":
+            return Net()
+        if self.model_name == "resnet50":
+            return ResNet50()
+        else:
+            raise NotImplementedError("Model not implemented")
+
+    def init_transform(self):
+        if self.model_name == "basic_cnn":
+            return data_transforms
+        if self.model_name == "resnet50":
+            return data_transforms_resnet
+        else:
+            raise NotImplementedError("Transform not implemented")
+
+    def get_model(self):
+        return self.model
+
+    def get_transform(self):
+        return self.transform
+
+    def get_all(self):
+        return self.model, self.transform
